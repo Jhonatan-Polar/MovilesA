215b45da3fa58dd7f5efd7aa0b86f952619a980a
GustavoRS642 <70769355+GustavoRS642@users.noreply.github.com>
2021-12-08 13:30:48 -0500

    Main Activity

--- app/src/main/java/pe/edu/pucp/proyecto/MainActivity.kt
+++ app/src/main/java/pe/edu/pucp/proyecto/MainActivity.kt
@@ -15,53 +15,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // Crear usuarios y libros
         crearUsuariosLibrosyResenias()
-
-        
 
     }
 
     private fun crearUsuariosLibrosyResenias(){
-        usuarios.add(1,Usuario("Gustavo Rojas" ,"Estudiante"))
-        usuarios.add(2,Usuario("Emerson Muñoz" ,"Docente"))
-        usuarios.add(3,Usuario("Jhonatan Cueva","Estudiante"))
-        usuarios.add(4,Usuario("Leandro Lazo"  ,"Docente"))
+        usuarios.add(Usuario("Gustavo Rojas" ,"Estudiante"))
+        usuarios.add(Usuario("Emerson Muñoz" ,"Docente"))
+        usuarios.add(Usuario("Jhonatan Cueva","Estudiante"))
+        usuarios.add(Usuario("Leandro Lazo"  ,"Docente"))
 
-        libros.add(1,Libro("Cálculo en Varias Variables (8.ª ed.)",
+        libros.add(Libro("Cálculo en Varias Variables (8.ª ed.)",
             "James Stewart","","Matemática"))
-        libros.add(2,Libro("Física Universitaria Vol. I",
+        libros.add(Libro("Física Universitaria Vol. I",
             "Francis W. Sears","","Física"))
-        libros.add(3,Libro("Química (primera edición)",
+        libros.add(Libro("Química (primera edición)",
             "Raymond Chang","","Química"))
-        libros.add(4,Libro("Modern Operating Systems",
+        libros.add(Libro("Modern Operating Systems",
             "Andrew Tanenbaum","","Informática"))
-        libros.add(5,Libro("Operating Systems: Design and Implementation",
+        libros.add(Libro("Operating Systems: Design and Implementation",
             "Andrew Tanenbaum","","Informática"))
-        libros.add(6,Libro("The C Programming Language",
+        libros.add(Libro("The C Programming Language",
             "Brian Kernighan & Dennis Ritchie","","Informática"))
-        libros.add(7,Libro("The C++ Programming Language",
+        libros.add(Libro("The C++ Programming Language",
             "Bjarne Stroustrup","","Informática"))
-        libros.add(8,Libro("Estructura de datos y métodos algorítmicos",
+        libros.add(Libro("Estructura de datos y métodos algorítmicos",
             "Narciso Martí Oliet","","Informática"))
 
-        libros[1].agregarResenia(usuarios[1],libros[1],
+        libros[0].agregarResenia(usuarios[0],libros[0],
             "¡Excelente libro! Tiene buenos ejercicios para Cálculo 3.")
-        libros[1].agregarResenia(usuarios[2],libros[1],
+        libros[0].agregarResenia(usuarios[1],libros[0],
             "Este libro me ayudó a pasar Cal3.")
-        libros[1].agregarResenia(usuarios[3],libros[1],
+        libros[0].agregarResenia(usuarios[2],libros[0],
             "Muy buen libro para Cálculo en Varias Variables.")
-        libros[8].agregarResenia(usuarios[4],libros[8],
+        libros[7].agregarResenia(usuarios[3],libros[7],
             "Este libro tiene buenos ejemplos para EDPM.")
-        libros[8].agregarResenia(usuarios[1],libros[8],
+        libros[7].agregarResenia(usuarios[0],libros[7],
             "Te explica bien la teoría para EDPM.")
-        libros[6].agregarResenia(usuarios[2],libros[6],
+        libros[5].agregarResenia(usuarios[1],libros[5],
             "¡Todo lo que se necesita para LP1 está aquí!")
-        libros[2].agregarResenia(usuarios[2],libros[2],
+        libros[1].agregarResenia(usuarios[1],libros[1],
             "Muy buen libro para Fa1 y Fa2. ¡Definitivamente lo recomiendo!")
-        libros[2].agregarResenia(usuarios[3],libros[2],
+        libros[1].agregarResenia(usuarios[2],libros[1],
             "Los ejemplos del libro ayudan a entender mucho los temas de Física.")
-
     }
 }
